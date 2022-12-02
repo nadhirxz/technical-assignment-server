8bf0d87dc18ac5441263905c68818068dd9b7ab3
Nadhir <nadhirxz@gmail.com>
2022-12-02 03:43:06 +0100

    Init model

--- server/server.go
+++ server/server.go
@@ -4,10 +4,12 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"gitlab.com/nadhirxz/technical-assignment-server/model"
 )
 
 func Init() {
 	router := gin.Default()
+	model.Init()
 
 	router.Run("localhost:" + os.Getenv("PORT"))
 }
