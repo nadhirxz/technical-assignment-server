d8e52408ba9938d08b3f36851bb2ba4a018f4d6c
Nadhir <nadhirxz@gmail.com>
2022-12-02 03:48:35 +0100

    Getting user by id

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gitlab.com/nadhirxz/technical-assignment-server/model"
@@ -23,6 +24,21 @@ func Init() {
 		}
 
 		c.JSON(http.StatusOK, users)
+	})
+
+	router.GET("/users/:id", func(c *gin.Context) {
+		id, _ := strconv.ParseUint(c.Param("id"), 10, 64)
+
+		user, err := model.GetUser(id)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"message": "User not found",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
 	})
 
 	router.Run("localhost:" + os.Getenv("PORT"))
