59b14958548c1bee378b6eb9988eacb411ab2033
Nadhir <nadhirxz@gmail.com>
2022-12-02 03:48:24 +0100

    Getting all users

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,19 @@ import (
 func Init() {
 	router := gin.Default()
 	model.Init()
+
+	router.GET("/users", func(c *gin.Context) {
+		users, err := model.GetUsers()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Internal server error",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, users)
+	})
 
 	router.Run("localhost:" + os.Getenv("PORT"))
 }
